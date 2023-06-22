ca46e4e761f281ea5537dd0955bc02a784385f8a
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-06-22 10:59:13 +0200

    change orga

--- backend/database.sql
+++ backend/database.sql
@@ -1,23 +1,16 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
-
 CREATE TABLE user (
   id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  nickname varchar(255) NOT NULL REQUIRED,
-  email varchar(255) NOT NULL REQUIRED,
-  passwrd varchar(255) NOT NULL REQUIRED,
+  nickname varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  passwrd varchar(255) NOT NULL,
   avatar_url varchar(255),
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE message (
   id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  message_date datetime NOT NULL REQUIRED,
-  message_text varchar(255) NOT NULL REQUIRED,
+  message_date datetime NOT NULL,
+  message_text TEXT NOT NULL,
   user_id int(11),
   FOREIGN KEY(user_id) REFERENCES user(id)
  ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
