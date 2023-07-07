df43b35bf0bab19027f079c970b6b630372f91cb
Roxane Belahcene <roxounette@aol.com>
2023-07-07 12:11:18 +0200

    css forum

--- frontend/src/pages/Forum.jsx
+++ frontend/src/pages/Forum.jsx
@@ -3,9 +3,9 @@ import users from "../services/users";
 export default function Forum() {
   return (
     <>
-      <h1>Forum</h1>
+      <h1 className="title"> Forum</h1>
       {users.map((user) => (
-        <div key={user.id}>
+        <div className="box" key={user.id}>
           <div>
             {user.name} - <span>{user.date}</span>
           </div>
