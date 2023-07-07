ee2f5756960f5cc1da995122012d921c0afee618
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-07-07 09:36:08 +0200

    add page contact admin

---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import Input from "../components/Input/Input";
+import Button from "../components/Button/Button";
+
+export default function Contact() {
+  const [message, setMessage] = useState({
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setMessage({ ...message, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.warn(message);
+  };
+
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+        quibusdam doloremque. Quisquam, quibusdam doloremque. Quisquam,
+        quibusdam doloremque.
+      </p>
+      <form onSubmit={handleSubmit}>
+        <Input
+          type="email"
+          info="Votre email"
+          id="email"
+          value={message.email}
+          hChange={handleChange}
+        />
+        <Input
+          type="text"
+          info="Sujet du message"
+          id="subject"
+          value={message.subject}
+          hChange={handleChange}
+        />
+        <Input
+          type="text"
+          info="Votre message"
+          id="message"
+          value={message.message}
+          hChange={handleChange}
+          textarea
+        />
+
+        <Button type="submit" text="Envoyer" />
+      </form>
+    </div>
+  );
+}
